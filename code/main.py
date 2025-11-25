bff8f6287c7d6b9686365b12ce7dceff6a55e243
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-11-25 02:32:03 -0300

    product query implemented

--- code/main.py
+++ code/main.py
@@ -1,26 +1,59 @@
 import asyncio
 from code.gpt_model import gpt_model
-from code.mcp import main
+from code.mcp import BrowserMCP, simple_implementation
 
 from langchain_core.messages import AIMessage
 
+
+async def main():
+
+    async with BrowserMCP() as browser_mcp:
+        snapshot = await browser_mcp.extract_snapshot(url="https://www.kabum.com.br/")
+
+        response: AIMessage = gpt_model.invoke(
+            f"""
+        Here is a page snapshot. Identify the search input bar
+        (where the user types the product name) and return ONLY the "ref"
+        value, nothing else.
+
+        Respond only the ref ID (ex: e315). No explanation.
+
+        Snapshot:
+        ```yaml
+            {snapshot}
+        ```
+        """
+        )
+
+        ref = response.content
+        print(f"Element ref={ref}")
+
+        snapshot = await browser_mcp.query_product(
+            url="https://www.kabum.com.br/",
+            input_ref=ref,
+            query="AMD Ryzen 7 5800X3D",
+        )
+
+        response: AIMessage = gpt_model.invoke(
+            f"""
+        You will receive a page snapshot from an ecommerce website.
+        Extract every product listed on the page.
+
+        Return a JSON array where each item has:
+        - name (string)
+        - price (number)
+
+        If the price is not present, omit the item.
+
+        Respond ONLY with valid JSON, nothing else.
+
+        Here's the snapshot:
+        {snapshot}
+        """
+        )
+
+        print(response.content)
+
+
 if __name__ == "__main__":
-
-    page_snapshot = asyncio.run(main(page_url="https://www.kabum.com.br/"))
-
-    response: AIMessage = gpt_model.invoke(
-        f"""
-    Here is a page snapshot. Identify the search input bar
-    (where the user types the product name) and return ONLY the "ref"
-    value, nothing else.
-
-    Respond only the ref ID (ex: e315). No explanation.
-
-    Snapshot:
-    ```yaml
-        {page_snapshot}
-    ```
-    """
-    )
-
-    print(f"Element ref={response.content}")
+    asyncio.run(main())
