0b7d300e28cb8acac6dc09941450dc7032e5b28c
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-11-25 16:23:23 -0300

    make_dir util func created

--- code/utils.py
+++ code/utils.py
@@ -1,10 +1,16 @@
+import base64
+import os
 from typing import Any, Dict, List
 
 import pandas as pd
 
 
-def write_into_xlsx(
-    output: List[Dict[str, Any]], filename: str = "output.xlsx"
-) -> None:
+def write_into_xlsx(output: List[Dict[str, Any]], filename: str) -> None:
     df = pd.DataFrame(output)
     df.to_excel(filename, index=False)
+
+
+def make_dir(dir_name: str, root_dir: str = "./output") -> str:
+    path = os.path.join(root_dir, dir_name)
+    os.makedirs(path, exist_ok=True)
+    return path
