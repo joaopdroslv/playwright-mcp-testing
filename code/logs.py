32a9fe11ed55905d4a7c0abbc20c51de52bc4b7f
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-11-25 14:00:56 -0300

    logs module created

---
+++ code/logs.py
@@ -0,0 +1,12 @@
+import logging
+
+log_format = (
+    "%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | "
+    "%(funcName)s | %(message)s"
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=log_format,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
