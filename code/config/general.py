1a535e63578272224735cdbd906290becdef6f22
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-12-03 10:33:52 -0300

    general.py config script created

---
+++ code/config/general.py
@@ -0,0 +1,8 @@
+from code.schemas.general import TargetSite
+
+ITEMS_WORKFLOW_SITES = [
+    TargetSite(
+        name="samburaimoveis",
+        url="https://www.samburaimoveis.com.br/busca_avancada?finalidade=Venda",
+    ),
+]
